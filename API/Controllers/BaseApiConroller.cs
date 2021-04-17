d4f17d63f90e31758f2c9e8bc85eb7e809aafbb6
mouldadam <mouldadam@hotmail.com>
2021-04-17 20:25:02 +0100

    End Of Section 2

---
+++ API/Controllers/BaseApiConroller.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace _API.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class BaseApiConroller : ControllerBase
+    {
+        
+    }
+}
